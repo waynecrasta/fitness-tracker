8bdac7813573b99dc07a0ae26faa2f71f4ece951
Wayne Crasta <wayne.crasta@gmail.com>
2020-12-02 22:11:45 -0600

    mysql db template

--- app/db.py
+++ app/db.py
@@ -1,5 +1,12 @@
 from app import app
 from flask_sqlalchemy import SQLAlchemy
+import os
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'ENTER DB HERE'
+
+MYSQL_USERNAME = os.getenv('MYSQL_USERNAME')
+MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
+MYSQL_HOST = os.getenv('MYSQL_HOST')
+MYSQL_DBNAME = os.getenv('MYSQL_DBNAME')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://{MYSQL_USERNAME}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DBNAME}'
 db = SQLAlchemy(app)
