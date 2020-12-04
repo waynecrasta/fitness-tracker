9f27ec667b9ea3c16429cfc9a8c5f78b557cbdc0
Wayne Crasta <wayne.crasta@gmail.com>
2020-12-04 01:25:59 -0600

    Migrate from MySQL to Postgres

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import config
+from flask_migrate import Migrate
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = config.SECRET_KEY
@@ -8,6 +10,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = config.SQLALCHEMY_DATABASE_URI
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from app.views import mod_auth as auth_mod
 app.register_blueprint(auth_mod)
